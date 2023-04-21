4d06ea8bf1ffa828c9c474592155c84480a36374
DINESH-39 <727821tuec039@skct.edu.in>
2023-04-21 12:49:32 +0530

    database

---
+++ Day3-DatabaseConnection/src/main/java/com/example/database/Day3DatabaseConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3DatabaseConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3DatabaseConnectionApplication.class, args);
+	}
+
+}
---
+++ Day3-DatabaseConnection/src/main/java/com/example/database/Model/CarModel.java
@@ -0,0 +1,37 @@
+package com.example.database.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="carmodel")
+public class CarModel {
+	
+	@Id
+	@Column(name="cid")
+	private int cid;
+	private String cname;
+	private int cmodel;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public int getCmodel() {
+		return cmodel;
+	}
+	public void setCmodel(int cmodel) {
+		this.cmodel = cmodel;
+	}
+	
+
+}
